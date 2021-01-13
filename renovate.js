daeecae6494ccbdb6d35a085b1a35d4b4a4eb100
Elliot Murphy <statik@users.noreply.github.com>
2021-01-13 11:08:51 -0500

    enable renovate.js
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

---
+++ renovate.js
@@ -0,0 +1,32 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+module.exports = {
+  extends: ["config:base", ":semanticCommitsDisabled"],
+  prConcurrentLimit: 30,
+  prHourlyLimit: 30,
+  prCreation: "not-pending",
+  pinDigests: true,
+  onboarding: true,
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  logLevel: "debug",
+  requireConfig: false,
+  platform: "github",
+  token: process.env.APP_TOKEN,
+  reviewersFromCodeOwners: true,
+  enabledManagers: ["github-actions", "npm", "python", "java"],
+  "github-actions": {
+    fileMatch: ["^\\.github/workflows/[^/]+\\.ya?ml$"],
+    pinDigests: true,
+    labels: ["dependencies", "github"],
+  },
+  npm: {
+    labels: ["dependencies", "javascript"],
+  },
+  packageRules: [
+    {
+      packageNames: ["renovate"],
+      schedule: ["before 3am on monday"],
+    },
+  ],
+};
